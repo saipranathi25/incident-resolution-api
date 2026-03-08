9b51567055cb1d90801366851740a57cec3b6920
saipranathi25 <vaddinenipranathi@gmail.com>
2026-03-08 14:01:05 +0530

    fixed server start

--- main.py
+++ main.py
@@ -23,3 +23,8 @@ def get_resolution(incident_id: str):
         return incidents[incident_id]
 
     return {"message": "Incident not found"}
+
+import uvicorn
+
+if __name__ == "__main__":
+    uvicorn.run("main:app", host="0.0.0.0", port=10000)
